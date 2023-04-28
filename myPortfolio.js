e521c8079a3de9ba5409d4e7a3d3a635926190bd
dickysexix <62500461+dickysexix@users.noreply.github.com>
2023-04-28 10:08:37 +0100

    a simple portfolio website

---
+++ myPortfolio.js
@@ -0,0 +1,56 @@
+const nextEl = document.querySelector('#nextEl');
+const prevEl = document.querySelector('#prevEl');
+const sliderEl = document.querySelectorAll('.slider');
+let position = 0;
+let index
+
+console.log(sliderEl.length);
+//slides effect
+function slides() {
+    if (position > 5 || position < 0) {
+        position = 0
+    }
+    for (let i = 0; i < sliderEl.length; i++) {
+        index = i;
+        // console.log(index);
+        sliderEl.forEach(slider => {
+            if (position == index) {
+                sliderEl[position].style.display = "block";
+                // console.log(index);
+            } else {
+                sliderEl[index].style.display = "none";
+            }
+        });
+    }
+
+}
+slides();
+
+nextEl.addEventListener('click', () => {
+    position++;
+    slides();
+    console.log(position);
+});
+prevEl.addEventListener('click', () => {
+    position--;
+    slides();
+    console.log(position);
+});
+
+//header effect
+const navButton = document.querySelector('.ham');
+const navList = document.getElementById('lister');
+console.log(navButton.innerHTML);
+
+navButton.addEventListener('click', () => {
+    if (navButton.innerHTML == "=") {
+        navList.style.display = "flex";
+        navList.style.transition = "0.2s ease-in-out";
+        navButton.innerHTML = "❌";
+    } else {
+        navList.style.transition = "0.2s ease-in-out";
+        navList.style.display = "none";
+        navButton.innerHTML = "=";
+
+    }
+})
