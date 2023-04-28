98a9879cfca5547c81f57208998a70d2b8142b48
dickysexix <62500461+dickysexix@users.noreply.github.com>
2023-04-28 21:34:34 +0100

    added javascript functions

--- myPortfolio.js
+++ myPortfolio.js
@@ -1,3 +1,23 @@
+//header effect
+const navButton = document.querySelector('.ham');
+const navList = document.querySelector('.lister');
+console.log(navButton.innerHTML);
+
+navButton.addEventListener('click', () => {
+    if (navButton.innerHTML == "=") {
+
+        navList.style.display = "flex";
+        navList.style.transition = "0.2s ease-in-out";
+        navButton.innerHTML = "❌";
+        console.log("click")
+    } else {
+        navList.style.transition = "0.2s ease-in-out";
+        navList.style.display = "none";
+        navButton.innerHTML = "=";
+
+    }
+})
+
 const nextEl = document.querySelector('#nextEl');
 const prevEl = document.querySelector('#prevEl');
 const sliderEl = document.querySelectorAll('.slider');
@@ -36,21 +56,3 @@ prevEl.addEventListener('click', () => {
     slides();
     console.log(position);
 });
-
-//header effect
-const navButton = document.querySelector('.ham');
-const navList = document.getElementById('lister');
-console.log(navButton.innerHTML);
-
-navButton.addEventListener('click', () => {
-    if (navButton.innerHTML == "=") {
-        navList.style.display = "flex";
-        navList.style.transition = "0.2s ease-in-out";
-        navButton.innerHTML = "❌";
-    } else {
-        navList.style.transition = "0.2s ease-in-out";
-        navList.style.display = "none";
-        navButton.innerHTML = "=";
-
-    }
-})
